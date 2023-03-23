b626d2118ae197f4d7e80bbcf09684a88fc8a53e
jonfra165 <76876808+jonfra165@users.noreply.github.com>
2023-03-23 11:50:43 +0100

    show-image
    Add a folder (images) in server
    Add a Component ShowImage that fetch data from server and show the image.

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import ShowImage from './components/ShowImage';
 
 function App() {
-
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
   return (
-    <>
-          <Button variant="info">Info</Button>{' '}
-
-    <div className="App">
-      
-      {(typeof data.members === "undefined") ? (
-        <p>Loading..</p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
+    <div>
+        <ShowImage />
     </div>
-    </>
   );
 }
 
--- server/server.py
+++ server/server.py
@@ -1,11 +1,23 @@
 from flask import Flask;
 import base64
+import cv2
+import numpy as np
+
 
 app = Flask(__name__)
 
-@app.route('/members')
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+@app.route('/show-image')
+def show_image():
+    img = cv2.imread('server/images/scoccish.png')
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, thresh_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
+    new_img = cv2.resize(thresh_img, (500, 700))
+
+    # Encode the image data as a base64 string
+    retval, buffer = cv2.imencode('.png', new_img)
+    img_str = base64.b64encode(buffer).decode()
+
+    return {'image': img_str }
 
 @app.post('/upload-image')
 def upload_image():
