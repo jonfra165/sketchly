78c5664147fe6572389f6b801c8e0616385975e1
jonfra165 <76876808+jonfra165@users.noreply.github.com>
2023-03-30 09:06:48 +0200

    Merge branch 'feature/first-request-API' into jonna-develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ShowImage from './components/ShowImage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+        <Navbar />
         <ShowImage />
     </div>
   );
